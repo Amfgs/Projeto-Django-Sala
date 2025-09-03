e1aa1c3cf57caa896cfb2f82e8c7f3f007336a3b
Arthur Moury <amfgs@cesar.school>
2025-09-03 11:45:21 -0300

    Model  - Pergunta

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+import datetime 
+from django.utils import timezone
 
 # Create your models here.
+class Pergunta(models.Model):
+    titulo = models.CharField(max_length=200, null=False)
+    detalhe = models.TextField(null=False)
+    tentativa = models.TextField()
+    data_criacao = models.DateTimeField("criado em")
+    usuario = models.CharField(max_length=200, null=False, default="anonimo")
+
+    def __str__(self):
+        return "[" + str(self.id) + "]" + self.titulo
+    def foi_publicada_recentemente(self):
+        return self.data_criacao >= timezone.now() - datetime.timedelta(days=1)
+    def strings_detalhada(self):
+        return "id: " +str(self.id) + "; titulo : " +self.titulo + "; detalhe: " +self.detalhe + "; tentativa: " + self.tentativa
+    
+class Resposta(models.Model):
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    texto = models.TextField(null=False)
+    votos = models.IntegerField(null=False)
+    data_criacao = models.DateTimeField("criado em")
+    usuario = models.CharField(max_length=200, null=False, default="anonimo")
+
+    def __str__(self):
+        return "[" + str(self.id) + "]" + self.texto
+    def foi_publicada_recentemente(self):
+        return self.data_criacao >= timezone.now() - datetime.timedelta(days=1)
